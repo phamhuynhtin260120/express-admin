0013ca657466f01ef872f6269f29a1f2e1282333
PhamHuynhTin <phamhuynhtin260120@gmail.com>
2026-07-13 15:35:26 +0700

    Create Factory, Model, Migrate team

---
+++ app/Models/Team.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\TeamFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    /** @use HasFactory<TeamFactory> */
+    use HasFactory;
+}
